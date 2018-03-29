d7108d449442419667627ebfa13bac1531fdd271
Edwin Gerard <edwin.gerard1@gmail.com>
2018-03-29 16:23:37 +0200

    Premier ajout de la page liste des series

---
+++ src/Controller/SerieController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aragorn
+ * Date: 29/03/18
+ * Time: 11:49
+ */
+
+namespace Controller;
+
+use Model\Serie;
+use Model\SerieManager;
+
+class SerieController extends AbstractController
+{
+    /**
+     * Display serie listing
+     *
+     * @return string
+     */
+    public function list()
+    {
+        $serieManager = new SerieManager();
+        $series = $serieManager->selectAll();
+
+        return $this->twig->render('Serie/list.html.twig', ['series' => $series]);
+    }
+
+}
