72537c5767bab39e6ed10f491998716e65218afd
Edwin Gerard <edwin.gerard1@gmail.com>
2018-04-19 17:42:40 +0200

    Contrôle de l'existence de la saison

--- src/Model/SeasonManager.php
+++ src/Model/SeasonManager.php
@@ -21,15 +21,33 @@ class SeasonManager extends AbstractManager
         parent::__construct(self::TABLE);
     }
 
+    /**
+     * @param array $data
+     * @return string|void
+     * @throws \Exception
+     */
     public function insert (array $data)
     {
         $idSerie = $data[ 'idSerie' ];
         $nb = $data[ 'nbSeasons' ];
-        $query = "INSERT INTO $this->table (numero_season, idserie) VALUES (:numb, :idSerie)";
-        $statement = $this->pdoConnection->prepare($query);
-        $statement->bindValue('numb', $nb);
-        $statement->bindValue('idSerie', $idSerie);
-        $statement->execute();
+        $verif = "SELECT numero_season FROM $this->table WHERE idserie = $idSerie";
+        $seasonInDb = $this->pdoConnection->query($verif, \PDO::FETCH_CLASS, $this->className)->fetchAll();
+        $errorMsg = '';
+        for ($i = 0; $i < count($seasonInDb); $i++) {
+            if ($nb == $seasonInDb[$i]->numero_season) {
+                $errorMsg = 'La saison existe déjà';
+            }
+        }
+        if ($errorMsg == '') {
+            $query = "INSERT INTO $this->table (numero_season, idserie) VALUES (:numb, :idSerie)";
+            $statement = $this->pdoConnection->prepare($query);
+            $statement->bindValue('numb', $nb);
+            $statement->bindValue('idSerie', $idSerie);
+            $statement->execute();
+        }else {
+            throw new \Exception($errorMsg);
+        }
+
 
     }
 }
