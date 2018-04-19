6a93daaea164f89b4a441f51d03f4fa478878450
Edwin Gerard <edwin.gerard1@gmail.com>
2018-04-19 15:43:41 +0200

    Ajout methode insert EpisodeManager

--- src/Model/EpisodeManager.php
+++ src/Model/EpisodeManager.php
@@ -9,7 +9,34 @@
 namespace Model;
 
 
-class EpisodeManager
+class EpisodeManager extends AbstractManager
 {
+    const TABLE = 'episode';
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+
+    public function insert (array $data)
+    {
+        $idSerie = $data['idSerie'];
+        $nbSeason = $data['numeroSeason'];
+        $query = "SELECT id FROM season WHERE numero_season = $nbSeason";
+        $result = $this->pdoConnection->query($query, \PDO::FETCH_ASSOC)->fetch();
+        $idSeason = $result['id'];
+        $query = "INSERT INTO $this->table (numero, title, broadcasting_date, idseason, idserie) VALUES (:nb, :title, :dateDiff, :idSeason, :idSerie)";
+        $statement = $this->pdoConnection->prepare($query);
+        $statement->bindValue('nb', $data['numeroEpisode']);
+        $statement->bindValue('title', $data['titleEpisode']);
+        $statement->bindValue('dateDiff', $data['date_diff']);
+        $statement->bindValue('idSerie', $idSerie);
+        $statement->bindValue('idSeason', $idSeason);
+        $statement->execute();
+
+    }
 
 }
